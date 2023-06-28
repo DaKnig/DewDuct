a58d9d8fecc7513ae679e089b8dd4907212b4d8a
DaKnig <37626476+DaKnig@users.noreply.github.com>
2023-06-28 22:57:33 +0300

    Minor changes to UI.
    - window title!
    
    - half transparent vid length!

--- src/config.rs
+++ src/config.rs
@@ -2,4 +2,4 @@ pub static VERSION: &str = "0.1.0";
 // pub static GETTEXT_PACKAGE: &str = "dewduct";
 // pub static LOCALEDIR: &str = "/app/share/locale";
 // pub static PKGDATADIR: &str = "/app/share/dewduct";
-pub static PKGNAME: &str = "dewduct";
+pub static PKGNAME: &str = "DewDuct";
